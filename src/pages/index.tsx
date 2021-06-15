2838d4adaaadbfed5deb1cb4ab458cd720b0001b
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-06-15 00:06:04 -0300

    chore: add technologies section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import {
   Button,
   Divider,
   Flex,
+  Grid,
   Heading,
   HStack,
   Image,
@@ -16,10 +17,13 @@ import {
   InputLeftElement,
   Icon,
   useCheckbox,
+  useMediaQuery,
 } from '@chakra-ui/react';
 
 export default function Home() {
   const [checked, setChecked] = useState(false);
+
+  const [isLargerThan375] = useMediaQuery('(min-width: 375px)');
 
   const { getInputProps, getCheckboxProps } = useCheckbox({
     onChange: () => setChecked(!checked),
@@ -384,6 +388,172 @@ export default function Home() {
               liberar no dia 18/06.
             </Text>
           </Box>
+        </Flex>
+      </Flex>
+
+      <Flex
+        as="section"
+        justifyContent="center"
+        position="relative"
+        padding={['80px 0', '160px 0 140px']}
+      >
+        <Flex
+          flexDirection={['column', 'column', 'column', 'row']}
+          justifyContent="space-between"
+          alignItems="center"
+          w="1120px"
+          m={['0 24px', '0 24px', '0 24px', 'unset']}
+        >
+          <Heading
+            as="h3"
+            fontSize={['30px', '48px']}
+            fontWeight="700"
+            lineHeight={['35px', '54px']}
+            color="gray.50"
+            textAlign={['center', 'center', 'unset']}
+            mb={['40px', '40px', '40px', 'unset']}
+            maxW={['unset', '357px']}
+          >
+            Evolua na tecnologia que faz mais sentido para você
+          </Heading>
+
+          <Grid
+            templateColumns={isLargerThan375 ? 'unset' : 'repeat(3, 1fr)'}
+            templateAreas={isLargerThan375 ? `'one' 'two' 'three'` : 'none'}
+            gap={isLargerThan375 ? 'unset' : '12px'}
+          >
+            <Box
+              display={isLargerThan375 ? 'flex' : 'grid'}
+              alignItems="center"
+              justifyContent="center"
+              gridArea={isLargerThan375 ? 'one' : 'unset'}
+            >
+              <Stack
+                direction={isLargerThan375 ? 'row' : 'column'}
+                spacing="12px"
+              >
+                <Image
+                  src="/technologies/react.svg"
+                  alt="React"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+
+                <Image
+                  src="/technologies/nodejs.svg"
+                  alt="NodeJs"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+
+                <Image
+                  src="/technologies/elixir.svg"
+                  alt="Elixir"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+
+                <Image
+                  src="/technologies/javascript.svg"
+                  alt="Javascript"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+
+                <Image
+                  src="/technologies/css.svg"
+                  alt="CSS"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+              </Stack>
+            </Box>
+            <Box
+              display={isLargerThan375 ? 'flex' : 'grid'}
+              alignItems="center"
+              justifyContent="center"
+              gridArea={isLargerThan375 ? 'two' : 'unset'}
+            >
+              <Stack
+                direction={isLargerThan375 ? 'row' : 'column'}
+                spacing="12px"
+              >
+                <Image
+                  src="/technologies/nextjs.svg"
+                  alt="NextJs"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+
+                <Image
+                  src="/technologies/expo.svg"
+                  alt="Expo"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+
+                <Image
+                  src="/technologies/html.svg"
+                  alt="HTML"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+
+                <Image
+                  src="/technologies/typescript.svg"
+                  alt="Typescript"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+              </Stack>
+            </Box>
+            <Box
+              display={isLargerThan375 ? 'flex' : 'grid'}
+              alignItems="center"
+              justifyContent="center"
+              gridArea={isLargerThan375 ? 'three' : 'unset'}
+            >
+              <Stack
+                direction={isLargerThan375 ? 'row' : 'column'}
+                spacing="12px"
+              >
+                <Image
+                  src="/technologies/phoenix.svg"
+                  alt="Phoenix"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+
+                <Image
+                  src="/technologies/flutter.svg"
+                  alt="Flutter"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+
+                <Image
+                  src="/technologies/express.svg"
+                  alt="Express"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+
+                <Image
+                  src="/technologies/dart.svg"
+                  alt="Dart"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+
+                <Image
+                  src="/technologies/others.svg"
+                  alt="Outras"
+                  w={['56px', '72px', 'unset']}
+                  h={['63px', '80px', 'unset']}
+                />
+              </Stack>
+            </Box>
+          </Grid>
         </Flex>
       </Flex>
     </main>
